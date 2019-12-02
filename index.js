656ab9c30d44d890167ab0fa1a7cc1a5d2b18e8c
0ctavia <octavia.couneson@gmail.com>
2019-12-02 16:56:24 +0100

    Username recuperation working

--- index.js
+++ index.js
@@ -11,15 +11,23 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-    socket.broadcast.emit('hi new user');
     console.log('a user connected');
-    
+    //récupérer le pseudonyme de l'utilisateur 
+    let pseudo = "";
+    const stockagepseudo = function(clientvar){
+    const pseudoserver = clientvar;
+    socket.emit('chat message', 'Hi '+ pseudoserver);
+    pseudo = pseudoserver;
+    };
+    //fonction lancée en cas d'event pseudo
+    socket.on('pseudo', stockagepseudo);
+    //ce qu'il se passe en cas de déconnection
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-
-    socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    // send chat messages back to the clientside
+    socket.on('chat message', function(msg, stockagepseudo){
+    io.emit('chat message', pseudo +" : " + msg);
     console.log('message: ' + msg);
     });
 });
