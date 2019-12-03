de3f1f8df44c877020ab2dc86b91fadd633a3445
Massimo Tascone <tascone.massimo@gmail.com>
2019-12-03 10:31:33 +0100

    fixing server error and separate css and js from html file

---
+++ public/assets/js/script.js
@@ -0,0 +1,10 @@
+const socket = io();
+$('form').submit(function(e){
+      e.preventDefault(); // prevents page reloading
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+});
+socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+});
