b01598e17216349c77c92af1fcad5ca2bb926e62
Octavia Couneson <octavia.couneson@gmail.com>
2019-12-05 15:33:05 +0100

    add link for drawing game

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const http = require('http');
+const http = require("http");
 var server = http.createServer(app);
-const io = require('socket.io').listen(server);
+const io = require("socket.io").listen(server);
 const port = process.env.PORT || 3000;
-const marked = require('marked');
-require('dotenv').config();
+const marked = require("marked");
+require("dotenv").config();
 // require d'apres le chemin de l'index du server
-const Content = require('./models/content')
+const Content = require("./models/content");
 //bodyparser is included in express
 //.urlencoded is a bodyparser stuff included in express now
-const drawing = io.of('/drawingPepeWithMyBFF.html');
+const drawing = io.of("/drawingPepeWithMyBFF.html");
 
 //connect to db
 mongoose
@@ -20,49 +20,49 @@ mongoose
     useUnifiedTopology: true
   })
   .then(test => {
-    console.log('Connectedbg');
+    console.log("Connectedbg");
   })
   .catch(err => console.log(err));
 
-
-
-server.listen(port, function () {
-  console.log('listening on *:3000');
+server.listen(port, function() {
+  console.log("listening on *:3000");
 });
 //ne pas oublier de dire au server ce qu'il va recevoir par ex du json
 app.use(express.json());
 // le server va servir les pages qui sont dans le folder public
-app.use(express.static('public'));
+app.use(express.static("public"));
 // comment recevoir des trucs de la banque de données
-app.get('/load', async (req, res, next) => {
-  const allContent = await Content.find()
+app.get("/load", async (req, res, next) => {
+  const allContent = await Content.find();
   await res.status(200).json({
     content: allContent
-  })
-  io.emit('dbqueryall', allContent);
+  });
+  io.emit("dbqueryall", allContent);
 });
 // comment envoyer des trucs à la base de donnée, récupérer le req.body puis tapper dans le model puis envoyer
-app.post('/send', async (req, res, next) => {
+app.post("/send", async (req, res, next) => {
   //créer un nouvel objet du meme type que le schema, qui respecte ça
+  const link =
+    "<a href='drawingPepeWithMyBFF.html' target='_blank'> CLICK ME</a>";
   if (req.body.content == "!drawing") {
-    res.json({
-      message: ("<a href='drawingPepeWithMyBFF.html'> CLICK ME</a>")
-    })
+    io.emit("sendLink", link);
+
+    console.log(link);
   }
   const nouvelobjet = await new Content({
     username: req.body.username,
     content: req.body.content
-  })
+  });
 
   await nouvelobjet.save();
   console.log(nouvelobjet);
   res.status(200).json({
-    message: 'objet créé',
+    message: "objet créé",
     objet: nouvelobjet
   });
 });
 
-app.delete('/:messageId', async (req, res) => {
+app.delete("/:messageId", async (req, res) => {
   const id = req.params.messageId;
   const deletedProduct = await Content.deleteOne({
     _id: id
@@ -70,39 +70,38 @@ app.delete('/:messageId', async (req, res) => {
   res.json({
     message: "C est delete mamen",
     deleted: deletedProduct
-  })
-})
-
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  //récupérer le pseudonyme de l'utilisateur 
-  let pseudo = "";
-  const stockagepseudo = function (clientvar) {
-    const pseudoserver = clientvar;
-    socket.emit('chat message', marked('*Bienvenue* ' + pseudoserver));
-    pseudo = pseudoserver;
-  };
-  //fonction lancée en cas d'event pseudo
-  socket.on('pseudo', stockagepseudo);
-  //ce qu'il se passe en cas de déconnection
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-  // send chat messages back to the clientside
-  socket.on('chat message', function (msg, stockagepseudo) {
-    io.emit('chat message', pseudo + " : " + marked(msg));
-    console.log('message: ' + msg);
   });
 });
 
-drawing.on('connection', function (socket) {
-  console.log('a player is here to play the best game ever');
-  socket.on('drawing', (x, y) => {
-    console.log("X :" + x + " Y: " +
-      y);
-    socket.broadcast.emit('sendDrawing', x, y);
-  })
-  socket.on('disconnect', () => {
-    drawing.emit('refreshCanvas');
-  })
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  //récupérer le pseudonyme de l'utilisateur
+  let pseudo = "";
+  const stockagepseudo = function(clientvar) {
+    const pseudoserver = clientvar;
+    io.emit("chat message", marked("*Bienvenue* " + pseudoserver));
+    pseudo = pseudoserver;
+  };
+  //fonction lancée en cas d'event pseudo
+  socket.on("pseudo", stockagepseudo);
+  //ce qu'il se passe en cas de déconnection
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  // send chat messages back to the clientside
+  socket.on("chat message", function(msg, stockagepseudo) {
+    io.emit("chat message", pseudo + " : " + marked(msg));
+    console.log("message: " + msg);
+  });
+});
+
+drawing.on("connection", function(socket) {
+  console.log("a player is here to play the best game ever");
+  socket.on("drawing", (x, y) => {
+    console.log("X :" + x + " Y: " + y);
+    socket.broadcast.emit("sendDrawing", x, y);
+  });
+  socket.on("disconnect", () => {
+    drawing.emit("refreshCanvas");
+  });
 });
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,50 +1,53 @@
 const socket = io();
 //fonction coté client pour récupérer la base de données, elle n'affiche pas encore l'objet reçu cependant
 const getdata = async () => {
-    const receive = await fetch("/load");
-    const receivejson = await receive.json();
-    console.log(receivejson);
+  const receive = await fetch("/load");
+  const receivejson = await receive.json();
+  console.log(receivejson);
 };
 getdata();
 
 //récupérer le pseudo de l'utilisateur
-let pseudo = window.prompt('Indiquez votre pseudo', 'anonymous');
+let pseudo = window.prompt("Indiquez votre pseudo", "anonymous");
 if (pseudo != "") {
-    console.log(pseudo);
-    socket.emit('pseudo', pseudo);
-};
+  console.log(pseudo);
+  socket.emit("pseudo", pseudo);
+}
 //récupérer les inputs du formulaire pour envoi au server
-$('form').submit(async function (e) {
-    e.preventDefault(); // eviter le reload
-    socket.emit('chat message', $('#m').val());
-    let message = $('#m').val();
-    console.log(message);
-    $('#m').val('');
-    //ceci va être recupéré serverside en faisant req.body.username par ex et fourré dans le modelschema
-    const data = {
-        username: pseudo,
-        content: message
-    };
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
-    //options est un objet
-    console.log(options);
-    const responseServer = await fetch('/send', options);
-    const object = await responseServer.json();
-    console.log(object);
+$("form").submit(async function(e) {
+  e.preventDefault(); // eviter le reload
+  socket.emit("chat message", $("#m").val());
+  let message = $("#m").val();
+  console.log(message);
+  $("#m").val("");
+  //ceci va être recupéré serverside en faisant req.body.username par ex et fourré dans le modelschema
+  const data = {
+    username: pseudo,
+    content: message
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  //options est un objet
+  console.log(options);
+  const responseServer = await fetch("/send", options);
+  const object = await responseServer.json();
+  console.log(object);
 
-    return false;
+  return false;
 });
-//récupérer l'event chat message emis par le server  
-socket.on('chat message', function (msg) {
+//récupérer l'event chat message emis par le server
+socket.on("chat message", function(msg) {
+  $("#messages").append($("<div>").html(msg));
+});
+socket.on("dbqueryall", allContent => {
+  //$('#messages').append($('<li>').text(msg));
+});
 
-    $('#messages').append($("<div>").html(msg));
-});
-socket.on('dbqueryall', (allContent) => {
-    //$('#messages').append($('<li>').text(msg));
+socket.on("sendLink", function(link) {
+  $("#messages").append($("<div>").html(link));
 });
